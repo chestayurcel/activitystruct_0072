83812f0b840e9885872cf270ec051dd31818e190
Yurcell <chestayurcel@gmail.com>
2024-04-01 14:38:57 +0700

    menambahkan output dan input untuk nama mahasiswa

--- strukturC_4.cpp
+++ strukturC_4.cpp
@@ -18,5 +18,7 @@ int main(){
     for (int i=0;i<3;i++){
         cout << "Nomor Mahasiswa : ";
         getline(cin, mhs[i].nim);
+        cout << "Nama Mahasiswa : ";
+        getline(cin, mhs[i].nama);
     }
 }
